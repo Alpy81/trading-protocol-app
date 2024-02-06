78978d742fce7c0c83d19abd01a6670c2201edcd
Alpy81 <alperen.ertas@gmx.de>
2024-02-06 14:41:33 +0100

    final commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ command }) => ({
+  plugins: [react()],
+  base: "/github.com/Alpy81/",
+  ...(command !== "serve" && { base: "/trading-protocol-app/" }),
+}));
